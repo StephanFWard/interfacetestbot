bdd535eac4f0149a4c1f49e8d0a1b4728492621d
Stephan F Ward <142859013+StephanFWard@users.noreply.github.com>
2024-01-06 12:13:43 -0500

    Create example.config.js

---
+++ example.config.js
@@ -0,0 +1,16 @@
+export default {
+    email: "x@x.com",
+    password: "abc123",
+    
+    // Each keyword means new search
+    keywordsList: ["Ruby", ".Ruby Developer", "Software Developer", "Software Engineer", "Java Developer", "Java Engineer"],
+    
+    // Job location e.g. "Worldwide", "European Union" and etc
+    location: "Remote", 
+    
+    // Job posting date e.g. PastWeek: "r604800"
+    date: "r604800",
+    
+    // Exclude jobs that have such keywords
+    keywordsBlackList: ["Jobot", "Cybercoders",]
+  }
